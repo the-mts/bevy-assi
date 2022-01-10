638a5409a02eb575e01a1d884eba1a44014eda02
Mohd Talib Siddiqui <talibmohd888@gmail.com>
2022-01-11 01:14:27 +0530

    v1

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Keyboard, ScrollView, StyleSheet, Text, View } from 'react-native';
+import TaskItem from './payment.js';
+import data from './data.json'
+
+const group_by_m = (data) => {
+  var final = new Object()
+  var ret = []
+  for( var x of data){
+    let g_name = x.date.slice(0,7)
+    if(g_name in final)
+      final[g_name].push(x)
+    else
+      final[g_name] = [x]
+  }
+  for(const i in final){
+    ret.push({title: i, data: final[i]})
+  }
+  console.log(ret)
+  return ret;
+}
+
+const mon = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 export default function App() {
+  group_by_m(data.sort((a,b) => b.date.localeCompare(a.date)));
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text style={styles.heading}>Mohd. Talib</Text>
+      <ScrollView style={styles.scrollView}>
+        {
+        data.map((rec, index) => {
+          return (
+            <View key={index} style={styles.taskContainer}>
+              <TaskItem index={index + 1} rec={rec}/>
+            </View>
+          );
+        })
+      }
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +46,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#181818',
   },
+  heading: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '600',
+    marginTop: 50,
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+  scrollView: {
+    marginBottom: 70,
+  },
+  taskContainer: {
+    marginTop: 20,
+  }
 });
---
+++ payment.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, } from "react-native";
+
+export default TaskItem = (props) => {
+    const mon = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    const get_i = (d) => {
+        return parseInt(d.slice(5,7))-1
+    }
+    const get_d = (d) => {
+        return d.slice(8,10)
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.taskContainer}>
+            <View style={styles.taskMainContainer}>
+            <Text style={styles.dateText}>{mon[get_i(props.rec.date)]+'\n'+get_d(props.rec.date)}</Text>
+            <View style={styles.taskSubContainer}>
+                <Text style={styles.task}>{props.rec.description}</Text>
+                <Text style={styles.paidBy}>Paid by {props.rec.paid_by}.</Text>
+            </View>
+            </View>
+                <Text style={props.rec.paid_by == 'you' ? styles.amtlent : styles.amtborr}>{props.rec.paid_by == "you" ? '+' : '-'} {props.rec.amount}</Text>
+            </View>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+    },
+    taskContainer: {
+        backgroundColor: '#323242',
+        borderRadius: 12,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flex: 1,
+        paddingLeft: 5,
+        paddingRight: 20,
+        paddingVertical: 5,
+        minHeight: 60,
+    },
+    taskSubContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flex: 1,
+    },
+    taskMainContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        flex: 1,
+    },
+    paidBy : {
+        color : '#bbb',
+        fontSize : 12,
+        width : '90%',
+    },
+    amtlent: {
+        fontSize : 18,
+        fontWeight : 'bold',
+        color : '#2a2'
+    },
+    amtborr: {
+        fontSize : 18,
+
+        fontWeight : 'bold',
+        color : '#c11'
+    },
+    task: {
+        fontWeight : 'bold',
+        color: '#eee',
+        width: '90%',
+        fontSize: 16,
+    },
+    dateText : {
+        fontWeight : 'bold',
+        color : '#b3b3b3',
+        fontSize : 15,
+        textAlign: 'center'
+    },
+});
